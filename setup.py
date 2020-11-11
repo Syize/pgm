9effd74bcd51740722e0d8de35e95260f12f3edd
Benjamin <benjaminevans316@gmail.com>
2020-11-11 13:05:35 +0000

    Name changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pypgm", # Replace with your own username
+    name="pgmreader", # Replace with your own username
     version="0.0.1",
     author="Benjamin Evans",
     author_email="19811799@sun.ac.za",
